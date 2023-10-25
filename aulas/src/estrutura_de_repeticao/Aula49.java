2efeffe2a12dde94be9bb1e641048562115042f8
devvagner <wagnerluizsg@outlook.com.br>
2023-10-25 15:39:18 -0300

    :sparkles: aula: estrutura de repetição for

---
+++ aulas/src/estrutura_de_repeticao/Aula49.java
@@ -0,0 +1,29 @@
+// Aula - 49 - Teste de mesa com estrutura repetitiva for
+
+package estrutura_de_repeticao;
+
+import java.util.Scanner;
+
+public class Aula49 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos números vão ser informados ? ");
+        int N = sc.nextInt();
+        int soma = 0;
+
+        for (int i = 0; i < N; i++) {
+            System.out.println("Digite um número: ");
+            int x = sc.nextInt();
+            soma += x;
+        }
+
+        System.out.println("soma total dos números: " + soma);
+        sc.close();
+
+        
+    }
+
+}
