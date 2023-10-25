6ec297b9be213eb0731d33ba5023e1e3b09c6c27
devvagner <wagnerluizsg@outlook.com.br>
2023-10-25 09:08:09 -0300

    :sparkles: aula: Expressões comparativas

---
+++ first_program/src/aula30.java
@@ -0,0 +1,30 @@
+// AULA - EXPRESSÕES COMPARATIVAS
+
+public class aula30 {
+
+    // > maior
+    // < menor
+    // >= maior ou igual
+    // <= menor ou igual
+    // == igual
+    // != diferente
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int  x = 5;
+
+        boolean c1 = x > 10;
+        boolean c2 = x > 0;
+        boolean c3 = x == 3;
+        boolean c4 = 10 <- 30;
+        boolean c5 = x != 2;
+
+        System.out.println("c1: " + c1);
+        System.out.println("c2: " + c2);
+        System.out.println("c3: " + c3);
+        System.out.println("c4: " + c4);
+        System.out.println("c5: " + c5);
+    }
+    
+}
