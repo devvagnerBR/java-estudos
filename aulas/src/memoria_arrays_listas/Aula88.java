1bddaae1f0ae79743e883fd7133dcad54031e125
devvagner <wagnerluizsg@outlook.com.br>
2023-10-27 10:25:27 -0300

    :sparkles: aula: referencia vs tipos valor

---
+++ aulas/src/memoria_arrays_listas/Aula88.java
@@ -0,0 +1,18 @@
+package memoria_arrays_listas;
+
+public class Aula88 {
+
+    public static void main(String[] args) {
+
+        int p;
+        p = 10;
+        System.out.println(p);
+
+        // === default values ===
+        // numbers: 0
+        // boolean: false
+        // char: \u0000 = 0
+        // object: null
+
+    }
+}
