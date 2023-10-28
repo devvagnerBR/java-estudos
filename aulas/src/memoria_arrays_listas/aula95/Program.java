ebee976e6c9c2f31b2131ad4508e87ca79bb5ebe
devvagner <wagnerluizsg@outlook.com.br>
2023-10-28 08:17:43 -0300

    :sparkles: Aula: Boxing, unboxinge wrapper classes

---
+++ aulas/src/memoria_arrays_listas/aula95/Program.java
@@ -0,0 +1,17 @@
+package memoria_arrays_listas.aula95;
+
+public class Program {
+
+    public static void main(String[] args) {
+            
+            int x = 20;
+            Object obj = x;
+    
+            System.out.println(x);
+    
+            int y = (int) obj;
+    
+            System.out.println(y);
+    }
+
+}
